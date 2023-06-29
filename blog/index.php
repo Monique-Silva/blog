69ea236106bdaa1c61c1e7c911954ef1836fe5ea
Monique-Silva <monique.silva-ximenes@le-campus-numerique.fr>
2023-06-29 12:14:21 +0200

    Ajout du dossier blog

---
+++ blog/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Mon blog</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Bienvenue sur le blog</h1>";
+    ?>
+</body>
+</html>
